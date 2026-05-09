4709c1b8a42f7ad2a07a2736f17c132e04f2ebb6
uknowdream <uknowdream@users.noreply.github.com>
2026-05-09 23:38:25 +0700

    Add Vercel deployment config

---
+++ api/index.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+$tmpPaths = [
+    getenv('VIEW_COMPILED_PATH') ?: '/tmp/views',
+    dirname(getenv('APP_CONFIG_CACHE') ?: '/tmp/config.php'),
+    dirname(getenv('APP_EVENTS_CACHE') ?: '/tmp/events.php'),
+    dirname(getenv('APP_PACKAGES_CACHE') ?: '/tmp/packages.php'),
+    dirname(getenv('APP_ROUTES_CACHE') ?: '/tmp/routes.php'),
+    dirname(getenv('APP_SERVICES_CACHE') ?: '/tmp/services.php'),
+];
+
+foreach (array_unique($tmpPaths) as $path) {
+    if ($path && ! is_dir($path)) {
+        mkdir($path, 0755, true);
+    }
+}
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
